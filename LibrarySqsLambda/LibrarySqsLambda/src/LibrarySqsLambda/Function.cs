c49fca9feb755dc5f1cb23f79499425a469d9252
AzizNorboev <130450647+Aziz-Norboev@users.noreply.github.com>
2023-04-24 17:31:51 +0500

    upload message to S3 implemented

--- LibrarySqsLambda/LibrarySqsLambda/src/LibrarySqsLambda/Function.cs
+++ LibrarySqsLambda/LibrarySqsLambda/src/LibrarySqsLambda/Function.cs
@@ -1,5 +1,9 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
+using System.Text;
+using Newtonsoft.Json;
+using Amazon.S3;
+using Amazon.S3.Transfer;
 
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -9,6 +13,7 @@ namespace LibrarySqsLambda
 {
     public class Function
     {
+        private const string _bucketName = "lambda-code-bucket-new";
         /// <summary>
         /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
         /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
@@ -29,20 +34,26 @@ namespace LibrarySqsLambda
         /// <returns></returns>
         public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
         {
-            context.Logger.LogInformation($"it's empty");
-            //if (evnt != null)
-            //{
-            //    foreach (var message in evnt.Records)
-            //    {
-            //        await ProcessMessageAsync(message, context);
-            //    }
-            //}
-            //else
-            //{
-            //    {
-            //        context.Logger.LogInformation($"it's empty");
-            //    }
-            //}
+            context.Logger.LogInformation($"Received {evnt.Records.Count} messages");
+
+            var s3Client = new AmazonS3Client();
+            var transferUtility = new TransferUtility(s3Client);
+
+            foreach (var message in evnt.Records)
+            {
+                // create JSON object with the message body
+                var jsonObject = new { Body = message.Body };
+
+                // serialize JSON object to string
+                var jsonString = JsonConvert.SerializeObject(jsonObject);
+
+                // upload JSON string to S3
+                var key = $"messages/{Guid.NewGuid()}.json";
+                var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+                await transferUtility.UploadAsync(memoryStream, _bucketName, key);
+
+                context.Logger.LogInformation($"Uploaded to S3 bucket: {key}");
+            }
         }
 
         private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
